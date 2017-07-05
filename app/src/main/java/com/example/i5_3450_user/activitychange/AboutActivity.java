ad21407cffac74fae1d77026ad1f84846f97feed
lonelyversemaker <xperiasesk17@gmail.com>
2017-07-05 12:06:29 +0500

    Initical commit

---
+++ app/src/main/java/com/example/i5_3450_user/activitychange/AboutActivity.java
@@ -0,0 +1,18 @@
+package com.example.i5_3450_user.activitychange;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by I5-3450-user on 05.07.2017.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+    }
+}
